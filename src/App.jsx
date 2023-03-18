e3ef2961d8a110f802872e2dc4adef55a91b695d
Tevita Palaki-Muti <palaki.tevita@yahoo.com>
2023-03-17 17:39:05 -0700

    Sum works well; added homework to create Multiplier

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+// import logo from './logo.svg'
+// import './App.css'
+
+import Button from '@material-ui/core/Button'
+
+import TextField from '@material-ui/core/TextField'
+import InboxIcon from '@material-ui/icons/Inbox'
+import { useState } from 'react'
+import Multiplier from './components/Multiplier'
+
+function App() {
+  const [number1, setNumber1] = useState(0)
+  const [number2, setNumber2] = useState(0)
+  const [sum, setSum] = useState(0)
+  const [whatever, setWhatever] = useState('whatever')
+
+  const calculate = () => {
+    const newSum = parseInt(number1) + parseInt(number2)
+    setSum(newSum)
+  }
+
+  const update1 = event => {
+    const { value } = event.target
+    setNumber1(value)
+  }
+
+  const update2 = event => {
+    const { value } = event.target
+    setNumber2(value)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload. HI HI
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Benjamin Graham equation V = ((EPS*(8.5+2g)*4.4)/(Y)) EPS = Earning per share <br />
+          Expected annual growth rate (2 over 7-10 years)= g "\n" Corporate bond yield 'risk free
+          rate' (Y) 8.5 price to Earnings Ratio for a no growth company intrinsic value per share =
+          V Margin of safty % =<b></b>
+        </a>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <div>
+          <InboxIcon /> Benjamin Graham equation V = ((EPS*(8.5+2g)*4.4)/(Y))
+        </div>
+        <div>
+          <InboxIcon /> EPS = Earning per share
+        </div>
+
+        <br />
+        <br />
+        <div>
+          A:{' '}
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            value={number1}
+            onChange={e => update1(e)}
+            size="small"
+          />
+        </div>
+
+        <br />
+        <div>
+          B:{' '}
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            value={number2}
+            onChange={e => update2(e)}
+            size="small"
+          />
+        </div>
+
+        <br />
+        <div>
+          Sum: <TextField id="outlined-basic" variant="outlined" value={sum} size="small" />
+        </div>
+
+        <Button variant="contained" color="primary" onClick={calculate}>
+          Calculate
+        </Button>
+
+        <div>
+          MULTIPLY
+          <Multiplier />
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Multiplier.jsx
@@ -0,0 +1,16 @@
+/**
+ * Make 3 text fields
+ * - Text Field 1 and 2 are the numbers to be multiplied
+ * - Text Field 3 is the result
+ * Add a button that says "Calculate" and can be clicked
+ */
+
+import { useState } from 'react'
+
+const Multiplier = props => {
+  const [number1, setNumber1] = useState(0)
+
+  return <div>hello</div>
+}
+
+export default Multiplier
